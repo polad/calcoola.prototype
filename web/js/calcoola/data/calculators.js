1190a376135a4ad5df5b8cd801356cbb1a04f10e
Polad Mirzayev <polad.mirzayev@gmail.com>
2013-11-26 07:36:46 +0000

    Added another Presure Gradient formula

--- web/js/calcoola/data/calculators.js
+++ web/js/calcoola/data/calculators.js
@@ -1,7 +1,7 @@
 define([], function() {
     return [{
-        name: "Pressure Gradient",
-        categories: [ "Basic Formulas", "Drilling Fluids" ],
+        name: "Pressure Gradient using mud weight, ppg",
+        categories: [ "Basic Formulas" ],
         args: {
             a: {
                 label: "Mud Weight",
@@ -11,6 +11,19 @@ define([], function() {
         uom: "psi/ft",
         formula: function(args) {
             return args.a * 0.052;
+        }
+    }, {
+        name: "Pressure Gradient using mud weight, lb/ft3",
+        categories: [ "Basic Formulas" ],
+        args: {
+            a: {
+                label: "Mud Weight",
+                uom: "lb/ft3"
+            }
+        },
+        uom: "psi/ft",
+        formula: function(args) {
+            return args.a * 0.006944;
         }
     }, {
         name: "Increase Mud Density",
