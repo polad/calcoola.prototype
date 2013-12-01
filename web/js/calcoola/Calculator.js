064b8376616fcd40d274d4160482b4567eac4196
Polad Mirzayev <polad.mirzayev@gmail.com>
2013-12-01 07:06:32 +0000

    Added description to Calculator Widget

--- web/js/calcoola/Calculator.js
+++ web/js/calcoola/Calculator.js
@@ -1,6 +1,7 @@
 define([
     "dojo/_base/declare",
     "dojo/_base/lang",
+    "dojo/dom-class",
     "dojox/lang/functional/object",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
@@ -8,14 +9,13 @@ define([
     "text!./templates/Calculator.html",
     "./Argument",
     "dijit/TitlePane"
-], function(declare, lang, objFunctions, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, WidgetTemplate, Argument) {
+], function(declare, lang, domClass, objFunctions, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, WidgetTemplate, Argument) {
     return declare([ _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin ], {
         templateString: WidgetTemplate,
         
         baseClass: "Calculator",
         
         calculator: null,
-        args: null,
         
         postCreate: function() {
             this.inherited(arguments);
@@ -23,20 +23,21 @@ define([
         },
         
         _setCalculatorAttr: function(calculator) {
-            this.calculator = calculator;
-            this.nameNode.set("title", calculator.name);
-            this.uomNode.innerHTML = calculator.uom;
+            calculator.name && (this.nameNode.set("title", calculator.name));
+            calculator.uom && (this.uomNode.innerHTML = calculator.uom);
+            this._setDescription(calculator.description);
             this._setupArgs(calculator.args);
+            this._set("calculator", calculator);
         },
         
         _setupArgs: function(args) {
-            this.args = {};
+            var argumentsObj = {};
             objFunctions.forIn(args, lang.hitch(this, function(argument, name) {
                 var widget = this._buildArgument(argument);
-                widget.placeAt(this.containerNode).
+                widget.placeAt(this.argumentContainer).
                     on("change", lang.hitch(this, function(newValue) {
-                        this.args[name] = newValue;
-                        this.calculate(this.args);
+                        argumentsObj[name] = newValue;
+                        this.calculate(argumentsObj);
                     }));
                 this.on("destroy", lang.hitch(widget, "destroy"));
             }));
@@ -56,6 +57,11 @@ define([
                 result = "";
             }
             this.resultNode.innerHTML = result;
+        },
+        
+        _setDescription: function(description) {
+            description && (this.descriptionNode.innerHTML = description);
+            domClass.toggle(this.descriptionNode, "hidden", !description);
         },
         
         onCalculate: function(result) {}
