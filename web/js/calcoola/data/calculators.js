41b85ac1b3637633834b7ba070431c86e29d0818
Polad Mirzayev <polad.mirzayev@gmail.com>
2013-12-02 06:14:15 +0000

    Added more "Basic Formulas"

--- web/js/calcoola/data/calculators.js
+++ web/js/calcoola/data/calculators.js
@@ -15,12 +15,12 @@ define([], function() {
         },
         description: "formula: psi/ft = mud weight, ppg x 0.052"
     }, {
-        name: "Pressure gradient using mud weight, lb/ft3",
+        name: "Pressure gradient using mud weight, lb/ft&sup3",
         categories: [ "Basic Formulas" ],
         args: {
             a: {
                 label: "Mud Weight",
-                uom: "lb/ft3",
+                uom: "lb/ft&sup3",
                 defaultValue: 0.0
             }
         },
@@ -28,10 +28,10 @@ define([], function() {
         formula: function(args) {
             return args.a * 0.006944;
         },
-        description: "formula: psi/ft = mud weight, lb/ft x 0.006944"
+        description: "formula: psi/ft = mud weight, lb/ft&sup3 x 0.006944"
     }, {
         name: "Hydrostatic pressure using ppg and feet as the units of measure",
-        categories: [ "Hydrostatic Pressure (HP)" ],
+        categories: [ "Basic Formulas" ],
         args: {
             a: {
                 label: "Mud Weight",
@@ -51,7 +51,7 @@ define([], function() {
         description: "formula: psi = mud weight, ppg x 0.052 x true vertical depth (TVD), ft"
     }, {
         name: "Hydrostatic pressure, psi, using pressure gradient, psi/ft",
-        categories: [ "Hydrostatic Pressure (HP)" ],
+        categories: [ "Basic Formulas" ],
         args: {
             a: {
                 label: "Pressure Gradient",
@@ -70,12 +70,12 @@ define([], function() {
         },
         description: "formula: psi = pressure gradient, psi/ft x true vertical depth, ft"
     }, {
-        name: "Hydrostatic pressure, psi, using mud weight, lb/ft3",
-        categories: [ "Hydrostatic Pressure (HP)" ],
+        name: "Hydrostatic pressure, psi, using mud weight, lb/ft&sup3",
+        categories: [ "Basic Formulas" ],
         args: {
             a: {
                 label: "Mud Weight",
-                uom: "lb/ft3",
+                uom: "lb/ft&sup3",
                 defaultValue: 0
             },
             b: {
@@ -88,10 +88,10 @@ define([], function() {
         formula: function(args) {
             return args.a * 0.006944 * args.b;
         },
-        description: "formula: psi = mud weight, lb/ft3 x 0.006944 x TVD, ft"
+        description: "formula: psi = mud weight, lb/ft&sup3 x 0.006944 x TVD, ft"
     }, {
         name: "Hydrostatic pressure, psi, using meters as unit of depth",
-        categories: [ "Hydrostatic Pressure (HP)" ],
+        categories: [ "Basic Formulas" ],
         args: {
             a: {
                 label: "Mud Weight",
@@ -109,6 +109,138 @@ define([], function() {
             return args.a * 0.052 * args.b * 3.281;
         },
         description: "formula: psi = mud weight, ppg x 0.052 x TVD, m x 3.281"
+    }, {
+        name: "Equivalent Circulating Density (ECD)",
+        categories: [ "Basic Formulas" ],
+        args: {
+            a: {
+                label: "Annular Pressure Loss",
+                uom: "psi",
+                defaultValue: 0
+            },
+            b: {
+                label: "True Vertical Depth",
+                uom: "ft",
+                defaultValue: 0
+            },
+            c: {
+                label: "Mud Weight",
+                uom: "ppg",
+                defaultValue: 0.0
+            }
+        },
+        uom: "ppg",
+        formula: function(args) {
+            return (args.a / 0.052 / args.b) + args.c;
+        },
+        description: "formula: ppg = ((annular pressure loss, psi ) / 0.052 / TVD, ft) + (mud weight in use, ppg)"
+    }, {
+        name: "Maximum Allowable Mud Weight from Leak-off Test Data",
+        categories: [ "Basic Formulas" ],
+        args: {
+            a: {
+                label: "Leak-Off Test Pressure",
+                uom: "psi",
+                defaultValue: 0
+            },
+            b: {
+                label: "Casing Shoe TVD",
+                uom: "ft",
+                defaultValue: 0
+            },
+            c: {
+                label: "Mud Weight",
+                uom: "ppg",
+                defaultValue: 0.0
+            }
+        },
+        uom: "ppg",
+        formula: function(args) {
+            return (args.a / 0.052 / args.b) + args.c;
+        },
+        description: "formula: ppg = (Leak-off Pressure, psi) ÷ 0.052 ÷ (Casing Shoe TVD, ft) + (mud weight, ppg)"
+    }, {
+        name: "Pump Out (PO) Triplex Pump, bbl/stk",
+        categories: [ "Basic Formulas" ],
+        args: {
+            a: {
+                label: "Liner Diameter",
+                uom: "in",
+                defaultValue: 0
+            },
+            b: {
+                label: "Stroke Length",
+                uom: "in",
+                defaultValue: 0
+            },
+            c: {
+                label: "Efficiency",
+                uom: "%",
+                defaultValue: 100
+            }
+        },
+        uom: "bbl/stk",
+        decimals: 5,
+        formula: function(args) {
+            return 0.000243 * Math.pow(args.a, 2) * args.b * (args.c/100);
+        },
+        description: "formula: bbl/stk = 0.000243 x (liner diameter, in.)&sup2 X (stroke length, in.)"
+    }, {
+        name: "Pump Out (PO) Triplex Pump, gpm",
+        categories: [ "Basic Formulas" ],
+        args: {
+            a: {
+                label: "Liner Diameter",
+                uom: "in",
+                defaultValue: 0
+            },
+            b: {
+                label: "Stroke Length",
+                uom: "in",
+                defaultValue: 0
+            },
+            c: {
+                label: "Strokes Per Minute",
+                uom: "SPM",
+                defaultValue: 0
+            }
+        },
+        uom: "gpm",
+        formula: function(args) {
+            return (3 * (Math.pow(args.a, 2) * 0.7854) * args.b) * 0.00411 * args.c;
+        },
+        description: "formula: gpm = [3 x ((liner diameter, in.)&sup2 x 0.7854) x (stroke length, in.)] 0.00411 x (strokes per minute)"
+    }, {
+        name: "Pump Out (PO) Duplex Pump, bbl/stk",
+        categories: [ "Basic Formulas" ],
+        args: {
+            a: {
+                label: "Liner Diameter",
+                uom: "in",
+                defaultValue: 0
+            },
+            b: {
+                label: "Stroke Length",
+                uom: "in",
+                defaultValue: 0
+            },
+            c: {
+                label: "Rod Diameter",
+                uom: "in",
+                defaultValue: 0
+            },
+            d: {
+                label: "Efficiency",
+                uom: "%",
+                defaultValue: 100
+            }
+        },
+        uom: "bbl/stk",
+        decimals: 5,
+        formula: function(args) {
+            return 0.000162 * args.b * (2 * Math.pow(args.a, 2) - Math.pow(args.c, 2)) * (args.d/100);
+        },
+        description: "formula: bbl/stk = 0.000162 x (stroke length, in.) x (2 x (liner diameter, in.)&sup2 - (rod diameter, in.)&sup2)"
     }, {
         name: "Increase Mud Density",
         categories: [ "Drilling Fluids" ],
